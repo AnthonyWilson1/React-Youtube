438c1cd66cbc1c8bd7aa7d97392a5ebcdf0ad9d7
Anthony Cortez <anthony1154@Anthony-Cortezs-MacBook-Pro.local>
2018-10-19 22:23:50 -0700

    Update

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
-const app = express();
-
-app.use(express.static(_dirname + '/'))
-
-app.listen(port);
-
-console.log('server started');
-
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
 
 
